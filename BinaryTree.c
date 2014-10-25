7ccabec7ee516aae85e8118c5ab9f7fdf8ad8309
Hanok <hanok_k92@yahoo.co.in>
2014-10-26 03:59:28 +0530

    Completed the operations on Binary search Tree

--- BinaryTree.c
+++ BinaryTree.c
@@ -4,129 +4,147 @@
 
 struct tree
 {
-	int value;
-	struct tree *left,*right;
+    int value;
+    struct tree *left,*right;
 };
 
 typedef struct tree node;
 
 void *Insert(node *root,node *newnode)
 {
-	if (root==NULL)
-	{
-		root=newnode;
-	}
-	else
-	{
-		if (root->value>newnode->value)
-		{
-			root->left=Insert(root->left,newnode);
-		}
-		else
-		{
-			root->right=Insert(root->right,newnode);
-		}
-	}
-	return root;
+    if (root==NULL)
+    {
+        root=newnode;
+    }
+    else
+    {
+        if (root->value>newnode->value)
+        {
+            root->left=Insert(root->left,newnode);
+        }
+        else
+        {
+            root->right=Insert(root->right,newnode);
+        }
+    }
+    return root;
 }
 
 void Print(node *root)
 {
-	if (!root)
+    if (!root)
+    {
+        return;
+    }
+    else
+    {
+        Print(root->left);
+        printf("%d\n",root->value);
+        Print(root->right);
+    }
+}
+
+node *InorderSuccessor(node *current)
+{
+	if (current->left==NULL)
 	{
-		return;
+		printf("Hola\n");
+		 return current;
 	}
 	else
 	{
-		Print(root->left);
-		printf("%d\n",root->value);
-		Print(root->right);
+		current=InorderSuccessor(current->left);
 	}
+	return current;
 }
 
 node *Delete(node *root,int val)
 {
-	//delete by using the inorder successor
-	//static bool flag;
-	if (root->value==val)
-	{
-		node *temp;	
-		temp=root;
-		if (root->right==NULL)
-		{
-			if (root->left==NULL)
-			{
-				root=NULL;
-			}
-			else
-			{
-				root=root->left;
-			}
-		}
-		else
-		{
-			if (root->right->left==NULL)
-			{
-				root=root->right;
-				root->left=temp->left;
-			}
-			else
-			{//this case is still pending
-				root=root->right->left;
-			}
-		}
-	}
-	else
-	{
-		if (root->value>val)
-		{
-			root->left=Delete(root->left,val);
-		}
-		else
-		{
-			root->right=Delete(root->right,val);
-		}
-	}
-	return root;
+    if (root->value==val)
+    {
+        if (root->left==NULL)
+        {
+            if (root->right==NULL)
+            {
+            	free(root);
+                root=NULL;
+            }
+            else
+            {
+             	root=root->right;   
+            }
+        }
+        else
+        {
+        	if (root->right->left==NULL)
+        	{
+        		root->right->left=root->left;
+        		root=root->right;
+        	}
+        		else
+       		{
+        		node *temp;
+ 	           	temp=InorderSuccessor(root->right);
+ 	           	printf("hey");
+                temp->left=root->left;
+                temp->right=root->right;
+                free(root);
+                root=temp;
+                root=Delete(root,temp->value);
+           	}
+        }
+    }
+    else
+    {
+        if (root->value>val)
+        {
+            root->left=Delete(root->left,val);
+        }
+        else
+        {
+            root->right=Delete(root->right,val);
+        }
+    }
+    return root;
 }
 
 int main()
 {
-	int n,val;
-	node *root;
-	node *newnode;
-	root=NULL;
-	while(1)
-	{
-	printf("Take any operation\n1.Insert\n2.Delete\n3.Print\n4.Exit");
-	scanf("%d",&n);
-	switch(n)
-	{
-		case 1:
-			printf("Enter the value\n");
-			scanf("%d",&val);
-			printf("\n");
-			newnode=(node *)malloc(sizeof(node));
-			newnode->value=val;
-			newnode->left=NULL;
-			newnode->right=NULL;
-			root=Insert(root,newnode);
-			Print(root);
-			break;
+    int n,val;
+    node *root;
+    node *newnode;
+    root=NULL;
+    while(1)
+    {
+    printf("Take any operation\n1.Insert\n2.Delete\n3.Print\n4.Exit");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+            printf("Enter the value\n");
+            scanf("%d",&val);
+            printf("\n");
+            newnode=(node *)malloc(sizeof(node));
+            newnode->value=val;
+            newnode->left=NULL;
+            newnode->right=NULL;
+            root=Insert(root,newnode);
+            Print(root);
+            break;
 
-		case 2:
-			printf("Enter the number to be deleted\n");
-			scanf("%d",&val);
-			root=Delete(root,val);
-			break;
+        case 2:
+            printf("Enter the number to be deleted\n");
+            scanf("%d",&val);
+            root=Delete(root,val);
+            break;
 
-		case 3:
-			Print(root);
-			break;
+        case 3:
+            Print(root);
+            break;
 
-		case 4:
-			exit(0);
-	}
-	}
-	return 0;
+        case 4:
+            exit(0);
+    }
+    }
+    return 0;
 }
