4cf570d558bd2f910535907b37511fd808e5df12
Hanok <hanok_k92@yahoo.co.in>
2018-02-15 15:46:58 +0000

    Initialise Heap datastructure

---
+++ heap.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#incldue <malloc.h>
+
+int a[100000],index;
+
+void percolate(){
+    for(){
+        
+    }
+}
+
+void Insert(value){
+    a[index] =  value;
+    index++;
+    percolate();
+}
+
+void Delete(){
+    
+}
+
+int main()
+{
+    int option;
+    printf("Heap 1. Insert \n2. Delete\n");
+    switch(option){
+        case 1:
+            Insert(value);
+        case 2:
+            Delete();
+    }
+}
