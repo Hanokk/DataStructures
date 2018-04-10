81557ebb2e52cbe558b7a5ee21ccbf1e0a0aa719
Hanok <hanok_k92@yahoo.co.in>
2018-04-10 07:11:16 +0000

    Heap CRUD implementation

--- heap.c
+++ heap.c
@@ -3,30 +3,77 @@
 
 int a[100000],index;
 
-void percolate(){
-    for(){
+int parent(int index){
+    return (index-1)/2;
+}
+
+int leftchild(int index){
+    return 2*index+1;
+}
+
+int rightchild(int index){
+    return 2*index+2;
+}
+
+void percolateup(int index){
+    if(index <= 0){
+        return;
+    }
+    if(a[parent(index)] > a[index]){
+        swap(parent(index),index);
+        percolateup(parent(index));
+    }
+    return;
+}
+
+void percolatedown(int index){
+    l = leftchild(index);
+    r = rightchild(index);
+    if(a[l] < a[index] || a[r] < a[index]){
+        if(a[l] < a[r]){
+            swap(l,index);
+            percolate(l);
+        }
+        else{
+            swap(r,index);
+            percolate(r);
+        }
+    }
+}
+
+void Insert(int value){
+    a[index] = value;
+    index++;
+    percolateup(index-1);
+}
+
+void Delete(int value){
+    int i;
+    while(i < index){
         
     }
 }
 
-void Insert(value){
-    a[index] =  value;
-    index++;
-    percolate();
-}
-
-void Delete(){
+int deleteMin(){
     
 }
 
 int main()
 {
-    int option;
-    printf("Heap 1. Insert \n2. Delete\n");
-    switch(option){
+    int option,value,Min;
+    printf("Heap 1. Insert \n2. Delete\n3. Get minimum");
+    switch(option) {
         case 1:
             Insert(value);
+            break;
         case 2:
-            Delete();
+            scanf("%d",&value);
+            Delete(value);
+            break;
+        case 3:
+            printf("Min is %d", a[0]);
+            break;
+        default:
+            break;
     }
 }
